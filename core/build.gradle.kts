6c9ed2a7f85a677d818dad0ad509b3531fbfa460
Detharon <detharon@gmail.com>
2025-11-22 07:45:06 +0100

    Remove jgoodies forms, unused dependency

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -17,5 +17,4 @@ dependencies {
     // TODO: Replace them with modern alternatives from Maven
     implementation(files("libs/gdx.jar"))
     implementation(files("libs/gdx-sources.jar"))
-    implementation(files("libs/forms-1.3.0.jar"))
 }
